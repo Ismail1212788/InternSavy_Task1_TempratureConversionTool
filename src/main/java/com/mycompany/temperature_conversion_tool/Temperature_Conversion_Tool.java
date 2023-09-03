c46d641427063289aa881b3ce99b22815f6e7fea
Muhammad Ismail <mi1936972@gmail.com>
2023-09-03 19:45:29 +0500

    initial commit

---
+++ src/main/java/com/mycompany/temperature_conversion_tool/Temperature_Conversion_Tool.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.temperature_conversion_tool;
+
+/**
+ *
+ * @author kali-i
+ */
+public class Temperature_Conversion_Tool {
+
+    public static void main(String[] args) {
+        DashBoard a=new DashBoard();
+        a.show();
+    }
+}
